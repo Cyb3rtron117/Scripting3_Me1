89d372604950463278ed7242c5e798fc1cf627d3
Cyb3rtron117 <dian.ehrke@gmail.com>
2026-04-15 11:11:39 +0200

    class demos

---
+++ class demos/demo2/myScript.js
@@ -0,0 +1,42 @@
+/*
+let email = "uhrbtfvurfb@gmail.com";
+let password = "ufwygerku";
+
+let tempstring = `Your Email is ${email} and your password is ${password}`;
+
+console.log(tempstring)
+
+
+let weapons = ["pistol", "Assault Rifle", "Sniper"];
+
+let boogers= ["one", "two"];
+
+weapons.push("Rocket Launcher", "Knife");
+
+console.log(weapons);
+
+let othrerboogers = weapons.concat(boogers);
+console.log(othrerboogers);
+
+
+let num1 = 8;
+let num2 = 15;
+
+let cuotients = num1 / num2
+let quotientString = cuotients.toString();
+
+console.log(quotientString);
+*/
+
+
+function concatenationFunction(stuff1, stuff2)
+{
+    let combined = stuff1.concat(stuff2);
+    return combined;
+}
+let string1 = "gvkuaseyjrvb";
+let string2 = " fbjhdfgbjk";
+
+let stuffs = concatenationFunction(string1, string2);
+
+console.log(stuffs);
